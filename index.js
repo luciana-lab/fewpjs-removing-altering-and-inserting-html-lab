e880d7a3dcd72e9df06a11fc7a2778c86e2e9617
Luciana Sugeta <lulusugeta@gmail.com>
2021-08-17 17:55:18 -0400

    commit tree b1492c39a889916cb875e59de7a9a62d1231d53b

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code here!
+// 1) no longer has DOM node 'main#main'
+    // let element = document.getElementById("main");
+    // element.remove(element);
+main.remove();
+
+// 2) has a 'newHeader' variable that points to node 'h1#victory'
+// 3) has a 'newHeader' variable that points to node 'h1#victory' Make sure you create an <h1> with id 'victory'
+// 4) has a 'newHeader' variable that points to node 'h1#victory' with "YOUR-NAME is the champion" inside
+// Reference: https://www.javascripttutorial.net/javascript-dom/javascript-createelement/
+let newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = 'YOUR-NAME is the champion'
+
+document.body.appendChild(newHeader);
